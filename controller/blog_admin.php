0c228b0217829732ed230c909c55e9b95be66add
dev <dev@dev5.bioshazard.com>
2014-03-25 20:01:11 -0400

    'mvc done'

--- controller/blog_admin.php
+++ controller/blog_admin.php
@@ -1,39 +1,16 @@
 <link href="%relbase%lf/apps/blog/css/styles.css" rel="stylesheet">
-
 <?php
-
+ 
 class blog_admin extends app
 {
 	public function main($vars)
 	{
 		$result = $this->db->fetchall('SELECT DISTINCT instance FROM io_threads ORDER BY instance');
-	
-		echo '<form class="instance-title" action="%appurl%addinst/" method="post"><input type="text" name="instance" placeholder="New Instance" /></form>';
-		if($result)
-		echo '<ul class="instance-list">';
-		foreach($result as $instance)
-		{
-			$instance = $instance['instance'];
-			echo '<li><a href="%appurl%inst/'.urlencode($instance).'/" class=".blog-instance">'.$instance.'</a><a href="%appurl%rminst/'.urlencode($instance).'/" class="delete-button">x</a></li>';
-		}
-		echo '</ul>';
-		/*
-		// No article selected
-		?>
-		<h3>[<a href="%appurl%blog/new/">Post New Article</a>]</h3>
-		<p>Select an article below to edit it.</p>
-		<ol>
-		<?php
-		$result = $this->db->query('SELECT id, title FROM io_threads ORDER BY id');
-		while($row = mysql_fetch_assoc($result))
-		{
-			echo '<li>[<a href="%appurl%blog/rm/'.$row['id'].'/">x</a>] <a href="%appurl%edit/'.$row['id'].'/">'.$row['title'].'</a></li>';
-		}
 		
-		?></ol><?php*/
+		include 'view/blog_admin.main.php';
 	}
 	
-	public function inst($vars)
+	public function inst($vars) // needs to be converted to _router(), home()
 	{
 		if(!isset($vars[1])) return $this->main();
 		
@@ -42,27 +19,16 @@ class blog_admin extends app
 		$this->inst = $inst;
 		$this->inst_base = $this->request->base.'apps/manage/blog/inst/'.urlencode($inst).'/';
 		
-		echo '<h3>Instance: <a href="'.$this->inst_base.'">'.$this->inst.'</a></h3>';
-		
 		$cats = $this->db->fetchall("SELECT DISTINCT category FROM io_threads WHERE instance = '".$inst."' ORDER BY category");
-		if(!$cats) redirect302($this->request->base.'apps/manage/blog/');
+		if(!$cats) 
+			redirect302($this->request->base.'apps/manage/blog/');
 		else
-		foreach($cats as $cat)
-			$this->cats[] = $cat['category'];
-			
-		echo '<div style="float: left; width: 200px;"><h4>Categories</h4>';
-		echo '<form action="'.$this->inst_base.'addcat/" method="post"><input type="text" name="category" placeholder="New category" /></form>';
-		foreach($this->cats as $cat)
-			echo '<a href="'.$this->inst_base.'cat/'.urlencode($cat).'/">'.$cat.'</a><br />';
-		echo '</div>';
-		echo '<div style="margin-left: 200px">';
+			foreach($cats as $cat)
+				$this->cats[] = $cat['category'];
 		
 		if(!isset($vars[0])) $vars[0] = 'view';
 		
-		$function = $vars[0];
-		$this->$function($vars);
-		
-		echo '</div>';
+		include 'view/blog_admin.home.php';
 	}
 	
 	private function cat($vars)
@@ -81,24 +47,7 @@ class blog_admin extends app
 		
 		$posts = $this->db->fetchall("SELECT id, title, category FROM io_threads WHERE".$where." instance = '".mysql_real_escape_string($inst)."' ORDER BY category, id DESC");
 		
-		// No article selected
-		?>
-		<p>Select an article below to edit it or [<a href="<?php echo $inst_base; ?>newarticle/<?php echo $category; ?>">Post New Article</a>]</p>
-		<ol>
-		<?php
-		
-		$cat = '';
-		foreach($posts as $post)
-		{
-			if($cat != $post['category'])
-			{
-				$cat = $post['category'];
-				echo '<h4>'.$cat.'</h4>';
-			}
-			
-			echo '<li>[<a onclick="return confirm(\'Do you really want to delete this?\');"  href="'.$inst_base.'rm/'.$post['id'].'/">x</a>] <a href="'.$inst_base.'edit/'.$post['id'].'/">'.$post['title'].'</a></li>';
-		}
-		?></ol><?php
+		include 'view/blog_admin.view.php';
 	}
 	
 	private function edit($vars)
@@ -139,19 +88,7 @@ class blog_admin extends app
 			$cat_options .= '<option'.$selected.' value="'.$cat.'">'.$cat.'</option>';
 		}
 		
-		?>
-		<form action="<?php echo $this->inst_base; ?>edit/<?=$row['id'];?>/" method="post">
-			<input type="submit" value="Save" /> <?=$msg;?> [<a href="<?php echo $inst_base; ?>">deselect post</a>]
-			<br /><br />
-			<input style="font-size: 22px; padding: 5px; width:100%" name="title" value="<?=htmlspecialchars($row['title'], ENT_QUOTES);?>" />
-			<br /><br />		
-			Category: <select name="category" id=""><?php echo $cat_options; ?></select> or <input type="text" name="newcat" placeholder="New Category" />
-			<br /><br />		
-			<textarea id="ckeditor" name="content"><?=htmlspecialchars($row['content'], ENT_QUOTES);?></textarea>
-			<br />
-			<input type="submit" value="Save" /> <?=$msg;?>
-		</form>
-		<?php
+		include 'view/blog_admin.edit.php';
 	}
 	
 	
@@ -193,18 +130,7 @@ class blog_admin extends app
 			$cat_options .= '<option'.$select.' value="'.$cat.'">'.$cat.'</option>';
 		}
 		
-		?>
-		<style type="text/css">
-			.add_thread .title { margin-bottom: 10px; padding: 5px; width: 100%; font-size:20px; }
-		</style>
-		<form action="<?php echo $inst_base; ?>create/" method="post" class="add_thread">
-			<input type="submit" class="submit" value="Post" /><br /><br />
-			<input type="text" name="title" value="New Title" class="title" />
-			Category: <select name="category" id=""><?php echo $cat_options; ?></select> or <input type="text" name="newcat" placeholder="New Category" /><br /><br />
-			<textarea id="ckeditor" name="content"></textarea>
-			<input type="hidden" name="access" value="public" />
-		</form>
-		<?php
+		include 'view/blog_admin.newarticle.php';
 	}
 	
 	private function rm($vars)
@@ -240,7 +166,7 @@ class blog_admin extends app
 	
 	public function rminst($vars)
 	{
-		echo 'Instance delete isnt implemented yet. Do remove an instance, delete all posts inside it.';
+		echo 'Instance delete isnt implemented yet. To remove an instance, delete all posts inside it.';
 		//$this->main();
 		//redirect302();
 		/*
