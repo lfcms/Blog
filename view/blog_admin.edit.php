6b4f5e0a50d9ae06558ab3c68dfa7ba7a1259237
dev <dev@dev.animalvsmachine.com>
2014-03-26 23:25:35 -0400

    'blog admin complete'

--- view/blog_admin.edit.php
+++ view/blog_admin.edit.php
@@ -1,10 +1,10 @@
-<form id="blog_admin_edit_form" action="<?=$this->inst_base;?>edit/<?=$row['id'];?>/" method="post">
+<form class="blog_edit_article" action="<?=$this->inst_base;?>edit/<?=$row['id'];?>/" method="post">
 	<div class="save_article">
 		<input type="submit" value="Publish Article" class="save_article" />
 	<?=$msg;?>
 	</div>
 	<div class="article_title">
-		<input style="font-size: 22px; padding: 5px; width:100%" name="title" value="<?=htmlspecialchars($row['title'], ENT_QUOTES);?>" />
+		<input name="title" value="<?=htmlspecialchars($row['title'], ENT_QUOTES);?>" />
 	</div>
 	<div class="category_selection">
 		Category: <select name="category" id=""><?=$cat_options;?></select> or <input type="text" name="newcat" placeholder="New Category" />
--- view/blog_admin.newarticle.php
+++ view/blog_admin.newarticle.php
@@ -1,11 +1,14 @@
-<style type="text/css">
-	.add_thread .title { margin-bottom: 10px; padding: 5px; width: 100%; font-size:20px; }
-</style>
-<form action="<?=$inst_base;?>create/" method="post" class="add_thread">
-	<input type="submit" class="submit" value="Post" />
-	<input type="text" name="title" value="New Title" class="title" />
-	Category: <select name="category" id=""><?=$cat_options;?></select>
-	or <input type="text" name="newcat" placeholder="New Category" />
+<form action="<?=$inst_base;?>create/" method="post" class="blog_edit_article">
+	<div class="save_article">
+		<input type="submit" class="submit" value="Post" />
+	</div>
+	<div class="article_title">
+		<input type="text" name="title" value="New Title" class="title" />
+	</div>
+	<div class="category_selection">
+		Category: <select name="category" id=""><?=$cat_options;?></select> or <input type="text" name="newcat" placeholder="New Category" />
+		<a href="<?=$inst_base;?>">cancel</a>
+	</div>
 	<textarea id="ckeditor" name="content"></textarea>
 	<input type="hidden" name="access" value="public" />
 </form>
