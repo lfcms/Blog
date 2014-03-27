58af3553940ba05caa6a556e56db87a0ad115947
dev <dev@dev.animalvsmachine.com>
2014-03-26 20:56:32 -0400

    'blog phase 3 complete'

--- view/blog_admin.edit.php
+++ view/blog_admin.edit.php
@@ -1,9 +1,14 @@
 <form id="blog_admin_edit_form" action="<?=$this->inst_base;?>edit/<?=$row['id'];?>/" method="post">
-	<input type="submit" value="Save" />
+	<div class="save_article">
+		<input type="submit" value="Publish Article" class="save_article" />
 	<?=$msg;?>
-	<a href="<?=$inst_base;?>">cancel</a>
-	<input style="font-size: 22px; padding: 5px; width:100%" name="title" value="<?=htmlspecialchars($row['title'], ENT_QUOTES);?>" />		
-	Category: <select name="category" id=""><?=$cat_options;?></select> or <input type="text" name="newcat" placeholder="New Category" />
+	</div>
+	<div class="article_title">
+		<input style="font-size: 22px; padding: 5px; width:100%" name="title" value="<?=htmlspecialchars($row['title'], ENT_QUOTES);?>" />
+	</div>
+	<div class="category_selection">
+		Category: <select name="category" id=""><?=$cat_options;?></select> or <input type="text" name="newcat" placeholder="New Category" />
+		<a href="<?=$inst_base;?>">cancel</a>
+	</div>
 	<textarea id="ckeditor" name="content"><?=htmlspecialchars($row['content'], ENT_QUOTES);?></textarea>
-	<input type="submit" value="Save" /> <?=$msg;?>
 </form>
