40cb68fea8b66388fced6b1936f5dbefe9c5d2e4
dev <dev@dev.eflipdomains.com>
2014-04-13 06:35:16 -0400

    'install'

--- install.sql
+++ install.sql
@@ -4,9 +4,9 @@ CREATE TABLE `io_threads` (
   `owner_id` int(5) NOT NULL,
   `instance` varchar(128) NOT NULL,
   `category` varchar(128) NOT NULL,
-  `title` varchar(100) NOT NULL,
+  `title` varchar(128) NOT NULL,
   `content` text NOT NULL,
-  `likes` int(5) NOT NULL,
+  `likes` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=31 DEFAULT CHARSET=latin1;
 
@@ -18,7 +18,7 @@ CREATE TABLE `io_messages` (
   `device` varchar(25) NOT NULL,
   `link` varchar(50) NOT NULL,
   `body` text NOT NULL,
-  `likes` int(5) NOT NULL,
+  `likes` int(11) NOT NULL,
   `reply` int(11) NOT NULL,
   PRIMARY KEY (`msg_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
