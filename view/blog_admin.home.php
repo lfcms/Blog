3a872b46eb42a30b1e09f1e5f61a710164e1fff1
Kripple <xpbxkidx@gmail.com>
2015-07-02 12:24:22 -0400

    Invalid argument supplied #69  fix
    Made it agnostic to a value or type.

--- view/blog_admin.home.php
+++ view/blog_admin.home.php
@@ -14,17 +14,27 @@
 				</form>
 			</div>
 			<div class="tile-content">
-				<?php foreach($cats as $cat): 
+ 				<?php if(isset($cats) && !is_null($cats)):
+ 					//agnosticsm
+ 					if(!is_array($cats)){
+ 						//make it an array
+ 						$cats = [$cats];
+ 					}
+					foreach($cats as $cat): 
 						if(isset($vars[1]) && $cat == $vars[1]) 
 							$selected = 'selected';
 						else 
 							$selected = '';
-				?>
-					<a class="blue button martop marbot <?=$selected;?>"
-							href="%appurl%cat/<?=urlencode($cat);?>/">
-						<?=$cat;?>
-					</a>
-				<?php endforeach; ?>
+					?>
+						<a class="blue button martop marbot <?=$selected;?>"
+								href="%appurl%cat/<?=urlencode($cat);?>/">
+							<?=$cat;?>
+						</a>
+					<?php endforeach; 
+				else: ?>
+					<a class="blue button martop marbot" href="#">Unknown </a>
+					
+				<?php endif;?>
 			</div>
 		</div>
 	</div>
